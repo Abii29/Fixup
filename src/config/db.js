f3bc2b49094899d1c9d744a1e1cd61b44ebe264b
Abilashini <abilashini2229a@gmail.com>
2025-03-10 16:11:29 +0530

    errors fixed

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,15 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected"); // Log the successful connection
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        
+        console.log(` MongoDB Connected to Atlas: ${conn.connection.host}`);
     } catch (error) {
-        console.error("Error connecting to MongoDB:", error.message);
-        process.exit(1); // Exit the process if MongoDB connection fails
+        console.error(`MongoDB Connection Error: ${error.message}`);
+        process.exit(1); // Exit process if connection fails
     }
 };
 
