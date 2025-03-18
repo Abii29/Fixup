ecad3636a8267424c602109b08571b987a2c2ab8
Anuda22 <anufernando2003@gmail.com>
2025-03-18 17:27:40 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProfilePage from './ProfilePage';
+import './ProfilePage.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ProfilePage />
+    </div>
+  );
+};
+
+export default App;
