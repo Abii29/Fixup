7a6d97a333819a2900314ef2ae5b786ed9d0bee4
Dasanayaka-max <vimethvansilu1@gmail.com>
2025-03-21 23:54:37 +0530

    mongoos

--- app/backend/firebase.js
+++ app/backend/firebase.js
@@ -1,11 +1,8 @@
 const admin = require('firebase-admin');
-
-
-const serviceAccount = require('C:\Users\Vimeth Vansilu\Desktop\SDGP\Fixup\fix-up-new-firebase-adminsdk-fbsvc-31cbb39283.json');
-
+const serviceAccount = require('./config/fix-up-new-firebase-adminsdk-fbsvc-31cbb39283.json');  
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount), 
+  credential: admin.credential.cert(serviceAccount),
 });
 
-console.log('Firebase to Admin SDK Initialized');
+console.log('Firebase Admin SDK Initialized');
