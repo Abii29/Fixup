2fcb1f33dd2ce9c8b91cb6c59305a3dc145faeba
Dasanayaka-max <vimethvansilu1@gmail.com>
2025-03-21 23:06:30 +0530

    firebase.js add

---
+++ app/backend/firebase.js
@@ -0,0 +1,11 @@
+const admin = require('firebase-admin');
+
+
+const serviceAccount = require('C:\Users\Vimeth Vansilu\Desktop\SDGP\Fixup\fix-up-new-firebase-adminsdk-fbsvc-31cbb39283.json');
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount), 
+});
+
+console.log('Firebase Admin SDK Initialized');
